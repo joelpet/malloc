d3fc86870f92b4d0158772f5b648b1d87c78847c
Joel Pettersson <petterssonjoel@gmail.com>
2011-10-11 01:34:06 +0200

    Add code docs to malloc.c.

--- malloc.c
+++ malloc.c
@@ -1,3 +1,11 @@
+/* malloc main
+ *
+ * This module contains the three library functions malloc(), free() and
+ * realloc(), together with a helper function morecore() that asks the system
+ * for more memory.
+ *
+ */
+
 #include "brk.h"
 #include "malloc.h"
 #include <stdbool.h>
@@ -5,16 +13,38 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/*
+ * The strategy (allocation algorithm) to use. Choose from:
+ * 1) First fit
+ * 2) Best fit
+ * 3) Worst fit
+ * 4) Quick fit
+ */
 #ifndef STRATEGY
 #define STRATEGY 1
 #endif
 
+/*
+ * The number of lists to use in the Quick fit algorithm implementation.
+ */
 #ifndef NRQUICKLISTS
 #define NRQUICKLISTS 4
 #endif
 
-static Header base; /* empty list to get started */
-static Header *freep = NULL; /* start of free list */
+/*
+ * The minimum #units to request when asking system for more memory.
+ */
+#define NALLOC 1024
+
+/*
+ * An empty list to get started.
+ */
+static Header base;
+
+/*
+ * Start of the free list, in non-Quick fit strategies.
+ */
+static Header *freep = NULL;
 
 /* Include the different malloc implementation source files here. */
 #include "first.c"
@@ -23,8 +53,16 @@ static Header *freep = NULL; /* start of free list */
 #include "quick.c"
 
 
-/* malloc: general-purpose storage allocator */
-void *malloc(size_t nbytes) {
+/* malloc
+ *
+ * malloc returns the start address of dynamically allocated memory.
+ * This is simply a wrapper function that calls the different algorithm
+ * implementations depending on the value of STRATEGY.
+ *
+ */
+void *malloc(
+    size_t nbytes) /* number of bytes of memory to allocate */
+{
     if (nbytes == 0) {
         return NULL;
     }
@@ -42,11 +80,14 @@ void *malloc(size_t nbytes) {
 #endif
 }
 
-#define NALLOC 1024
 
-/* minimum #units to request */
-/* morecore: ask system for more memory */
-static Header *morecore(unsigned nu)
+/* morecore
+ *
+ * morecore asks system for more memory and returns pointer to it.
+ *
+ */
+static Header *morecore(
+    unsigned nu) /* the amount of memory to ask for (in Header-sized units) */
 {
     char *cp;
     Header *up;
@@ -62,8 +103,12 @@ static Header *morecore(unsigned nu)
 }
 
 
-/* free: put block ap in free list */
-void free(void *ap)
+/* free
+ *
+ * Put block ap in free list.
+ */
+void free(
+    void *ap) /* pointer to data part of allocated memory block */
 {
 #if STRATEGY == 4
     int list_index;
@@ -81,6 +126,12 @@ void free(void *ap)
         return;
     }
 
+
+    /*
+     * For the Quick fit strategy, if allocated memory block has been stored in
+     * a quick list, simply put the whole block back and return.
+     */
+
 #if STRATEGY == 4
     list_index = get_quick_fit_list_index(sizeof(Header) * (bp->s.size - 1));
     if (list_index < NRQUICKLISTS) {
@@ -90,6 +141,12 @@ void free(void *ap)
         return;
     }
 #endif
+
+
+    /*
+     * Merge free memory blocks if the one being freed is adjacent to another
+     * free one,
+     */
 
     /* point to block header */
     for (p = freep; !(bp > p && bp < p->s.ptr); p = p->s.ptr)
@@ -114,8 +171,20 @@ void free(void *ap)
     freep = p;
 }
 
-void *realloc(void *ptr, size_t size) {
+/* realloc
+ *
+ * realloc changes size of the given memory block and returns pointer to new
+ */
+void *realloc(
+    void *ptr,      /* pointer to memory block to resize */
+    size_t size)    /* the new size, in bytes */
+{
     Header *bp, *p;
+
+    /*
+     * Take care of some simple, basic cases like when the given pointer is
+     * NULL or the size is non-positive.
+     */
 
     if (ptr == NULL) {
         return malloc(size);
@@ -128,10 +197,15 @@ void *realloc(void *ptr, size_t size) {
     unsigned int numbytes = sizeof(Header) * (bp->s.size - 1);
 
     if (size == numbytes) {
-        return ptr;
+        return ptr; /* stop and return here if no resize is necessary */
     }
 
-    /* mallockera mer minne och kopiera data eetc. */
+
+    /*
+     * Simply allocate as much memory as the new size and copy the old data
+     * into the new place. Then free the previously allocated memory.
+     */
+
     p = malloc(size);
     if (p == NULL) {
         return NULL;
@@ -140,6 +214,7 @@ void *realloc(void *ptr, size_t size) {
     if (size < numbytes) {
         numbytes = size;
     }
+
     memcpy(p, ptr, numbytes);
     free(ptr);
 
