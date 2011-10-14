f42ceb7e94e2aa1ed5cedf3800b65ca288f2e8b0
Joel Pettersson <petterssonjoel@gmail.com>
2011-10-14 16:52:02 +0200

    Fix bug in best.c and add docs.

--- best.c
+++ best.c
@@ -1,11 +1,28 @@
+/* best fit malloc
+ *
+ * This module contains the function that implements the Best fit malloc
+ * strategy, malloc_best().
+ *
+ */
 
-void *malloc_best(size_t nbytes)
+#include <limits.h>
+
+
+/* malloc_best
+ *
+ * malloc_best returns the start address of the newly allocated memory.
+ * It implements the Best fit algorithm, which tries to find the smallest free
+ * block that is large enough.
+ *
+ */
+void *malloc_best(
+    size_t nbytes) /* number of bytes of memory to allocate */
 {
     Header *p, *prevp;
     Header *moreroce(unsigned);
     unsigned nunits;
-    Header *minp, *minprevp;
-    bool found_fitting_block = false;
+    int min_size = INT_MAX;
+    Header *minp = NULL, *minprevp = NULL;
 
     nunits = (nbytes+sizeof(Header)-1)/sizeof(Header) + 1;
     if ((prevp = freep) == NULL) { /* no free list yet */
@@ -13,31 +30,31 @@ void *malloc_best(size_t nbytes)
         base.s.size = 0;
     }
 
-    minp = prevp;
+    minp = NULL;
+
+
+    /*
+     * Iterate over the free list and find the smallest block that is large
+     * enough to hold nbytes of data.
+     */
 
     for (p = prevp->s.ptr; ; prevp = p, p = p->s.ptr) {
         if (p->s.size >= nunits) { /* big enough */
             if (p->s.size == nunits) { /* exactly */
                 prevp->s.ptr = p->s.ptr;
+                freep = prevp;
+                return (void *)(p + 1);
             }
             else {
-                if (p->s.size < minp->s.size) {
-                    found_fitting_block = true;
+                if (minp == NULL || p->s.size < min_size) {
                     minp = p;
                     minprevp = prevp;
-                    continue;
+                    min_size = minp->s.size;
                 }
-                /* allocate tail end */
-                p->s.size -= nunits;
-                p += p->s.size;
-                p->s.size = nunits;
             }
-            freep = prevp;
-            /* return Data part of block to user */
-            return (void *)(p+1); 
         }
         if (p == freep) { /* wrapped around free list */
-            if (found_fitting_block) {
+            if (minp != NULL) {
                 /* allocate tail end */
                 minp->s.size -= nunits;
                 minp += minp->s.size;
@@ -45,8 +62,9 @@ void *malloc_best(size_t nbytes)
                 freep = minprevp;
                 return (void *)(minp + 1);
             }
-            if ((p = morecore(nunits)) == NULL)
+            if ((p = morecore(nunits)) == NULL) {
                 return NULL; /* none left */
+            }
         }
     }
 }
