47821a9a86baea6d91ed552b9deaabd6ba246537
Joel Pettersson <petterssonjoel@gmail.com>
2011-10-14 16:59:17 +0200

    Fix worst.c (stupid allocation) and add docs.

--- worst.c
+++ worst.c
@@ -1,11 +1,24 @@
+/* malloc_best
+ *
+ * This module contains the function that implements the Worst fit malloc
+ * strategy, malloc_worst().
+ *
+ */
 
-void *malloc_worst(size_t nbytes)
+/* malloc_worst
+ *
+ * malloc_worst returns the start address of the newly allocated memory.
+ * It implements the Worst fit algorithm, which tries to find a free memory
+ * block that is as large as possible.
+ *
+ */
+void *malloc_worst(
+        size_t nbytes) /* number of bytes of memory to allocate */
 {
     Header *p, *prevp;
     Header *moreroce(unsigned);
-    unsigned nunits;
-    Header *maxp, *maxprevp;
-    bool found_fitting_block = false;
+    unsigned nunits, max_size = 0;
+    Header *maxp = NULL, *maxprevp = NULL;
 
     nunits = (nbytes+sizeof(Header)-1)/sizeof(Header) + 1;
     if ((prevp = freep) == NULL) { /* no free list yet */
@@ -13,31 +26,29 @@ void *malloc_worst(size_t nbytes)
         base.s.size = 0;
     }
 
-    maxp = prevp;
+
+    /*
+     * Iterate over the free list and find the largest block, that is large
+     * enough to hold nbytes of data.
+     */
 
     for (p = prevp->s.ptr; ; prevp = p, p = p->s.ptr) {
         if (p->s.size >= nunits) { /* big enough */
             if (p->s.size == nunits) { /* exactly */
                 prevp->s.ptr = p->s.ptr;
+                freep = prevp;
+                return (void *)(p + 1);
             }
             else {
-                if (p->s.size > maxp->s.size) {
-                    found_fitting_block = true;
+                if (maxp == NULL || p->s.size > max_size) {
                     maxp = p;
                     maxprevp = prevp;
-                    continue;
+                    max_size = maxp->s.size;
                 }
-                /* allocate tail end */
-                p->s.size -= nunits;
-                p += p->s.size;
-                p->s.size = nunits;
             }
-            freep = prevp;
-            /* return Data part of block to user */
-            return (void *)(p+1); 
         }
         if (p == freep) { /* wrapped around free list */
-            if (found_fitting_block) {
+            if (maxp != NULL) {
                 /* allocate tail end */
                 maxp->s.size -= nunits;
                 maxp += maxp->s.size;
